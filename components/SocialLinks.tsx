8e0b4f71a463466d62a55e912bf8a2993f6e11c6
Srikant Loric <srikantloric@gmail.com>
2023-02-14 19:38:49 +0530

    dsdsdds

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
 import React from "react";
-import TwitterIcon from "../public/images/1x/twitter.png";
-import InstagramIcon from "../public/images/1x/ig.png";
-import FacebookIcon from "../public/images/1x/facebook.png";
+import Youtube from "../public/images/youtube.png";
+import Telegram from "../public/images/telegram.png";
+import Gmail from "../public/images/gmail.png";
 
 function SocialLinks() {
   return (
     <div className="social-links">
     <div className="circle">
-      <a href="https://www.google.com">
-        <Image src={TwitterIcon} alt="twittericon" fill ></Image>
+      <a href="https://www.youtube.com/@heyanand" target="_blank">
+        <Image src={Youtube} alt="twittericon" fill ></Image>
       </a>
     </div>
     <div className="circle">
-      <a href="https://www.google.com">
-        <Image src={FacebookIcon} alt="facebookicon" fill></Image>
+      <a href="https://t.me/heyyanand" target="_blank">
+        <Image src={Telegram} alt="facebookicon" fill></Image>
       </a>
     </div>
     <div className="circle">
-      <a href="https://www.google.com">
-        <Image src={InstagramIcon} alt="instagram" fill></Image>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=hey.anandverma@gmail.com" target="_blank">
+        <Image src={Gmail} alt="instagram" fill></Image>
       </a>
       </div>
   </div>
